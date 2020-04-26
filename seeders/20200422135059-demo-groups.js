ec7b4f7afdebda6e9fbe60e774de266594e0e288
Evgeny Kolesnikov <kolei@ya.ru>
2020-04-26 14:46:34 +0300

    добавил даты

--- seeders/20200422135059-demo-groups.js
+++ seeders/20200422135059-demo-groups.js
@@ -4,9 +4,9 @@
         up: (queryInterface, Sequelize) => {
             return queryInterface.bulkInsert(
                 'Groups',
-                [   {id: 1, name: 'И-31'},
-                    {id: 2, name: 'С-21'},
-                    {id: 3, name: 'И-21'} 
+                [   {id: 1, name: 'И-31', createdAt: new Date(), updatedAt: new Date()},
+                    {id: 2, name: 'С-21', createdAt: new Date(), updatedAt: new Date()},
+                    {id: 3, name: 'И-21', createdAt: new Date(), updatedAt: new Date()} 
                 ], {});
         },
 
